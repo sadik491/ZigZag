6be24413f4d7cf501aa183464c4b0ae572fd3c9e
Md Sadiqul Islam Khan <46827272+sadik491@users.noreply.github.com>
2020-02-17 06:37:16 +0600

    initial commit

---
+++ ZigZag/Assets/Scripts/BallControl.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+
+public class BallControl : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed;
+    bool started;
+    
+    float screenWidth;
+
+    void Start()
+    {
+        started = false;
+        screenWidth = Screen.width;
+    }
+
+   
+    void Update()
+    {
+
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.touchCount > 0)
+            {
+                if (Input.GetTouch(0).position.x < screenWidth)
+                {
+                    //left
+                    rb.velocity = new Vector3(speed * Time.deltaTime, 0, speed * Time.deltaTime);
+                }
+                else
+                {
+                    //right
+                    rb.velocity = new Vector3(-100f * Time.deltaTime, 0, speed * Time.deltaTime);
+                }
+            }
+        }
+
+        if (!started)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb.velocity = new Vector3(0, 0, speed * Time.deltaTime);
+                started = true;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            SwitchDirection();
+        }
+        
+
+    }
+
+    private void SwitchDirection()
+    {
+        if (rb.velocity.z > 0)
+        {
+            rb.velocity = new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+        else if (rb.velocity.x > 0)
+        {
+            rb.velocity = new Vector3(0, 0, speed * Time.deltaTime);
+        }
+    }
+}
